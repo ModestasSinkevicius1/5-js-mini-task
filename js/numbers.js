a8aa8cb2e57a2f205c82d19cc714c56dce525c62
ModestasSinkevicius1 <modestas.sinkevicius1@gmail.com>
2022-06-29 15:52:45 +0300

    added operations for numbers and text

---
+++ js/numbers.js
@@ -0,0 +1,15 @@
+// Kintamuju inicijavimas
+const a = 1;
+console.log(a);
+
+const b = 2;
+console.log(b);
+
+const c = 3;
+console.log(c);
+
+// Veiksmai su kintamaisiais
+const d = a + b + c;
+console.log('Suma:', d);
+
+
---
+++ js/strings.js
@@ -0,0 +1,13 @@
+// Kintamuju inicijavimas
+const a = 'dziaugsmingu';
+console.log(a);
+
+const b = ' sventu ';
+console.log(b);
+
+const c = 'kaledu!';
+console.log(c);
+
+// Veiksmai su kintamaisiais
+const d = a + b + c;
+console.log('Junginys:', d);
