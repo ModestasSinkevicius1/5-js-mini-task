d42e7a0d4fd26e33b2acf1e80a8a005154a49005
ModestasSinkevicius1 <modestas.sinkevicius1@gmail.com>
2022-06-30 15:59:13 +0300

    condition, cycle

---
+++ js/condition.js
@@ -0,0 +1,18 @@
+const a = 7;
+const b = 4;
+
+const c = (a > b) ? true : false;
+
+console.log(c);
+
+const num = 1;
+
+switch (num) {
+    case (1):
+        console.log('Yes');
+        break;
+
+    default:
+        console.log('No');
+        break;
+}
---
+++ js/cycle.js
@@ -0,0 +1,20 @@
+// Ciklo panaudojimas
+// Uzduotis 1
+let suma = 0;
+
+for(let i = -70;i<30;i++)
+{
+    suma += i;
+}
+console.log(suma);
+
+// Uzduotis 2
+
+let abc = 'abcdef';
+let newAbc = '';
+
+for(let i = abc.length-1;i >= 0;i--)
+{
+    newAbc += abc[i];
+}
+console.log(newAbc);
