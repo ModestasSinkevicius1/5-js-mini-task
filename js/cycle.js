bc3832df676bbe918612ac27e1f5da3cc0559879
ModestasSinkevicius1 <modestas.sinkevicius1@gmail.com>
2022-07-01 15:41:56 +0300

    completed cycle task

--- js/cycle.js
+++ js/cycle.js
@@ -18,3 +18,49 @@ for(let i = abc.length-1;i >= 0;i--)
     newAbc += abc[i];
 }
 console.log(newAbc);
+
+// Uzduotis 3
+
+let count = 0;
+
+const m1 = [3, 5, 7];
+
+for(let j = 0;j<3;j++)
+{
+    for(let i = 0;i <= 11; i++){
+        if((i % m1[j])===0)
+        {
+            count++;
+        }
+    }
+    console.log('Skaičių intervale tarp 0 ir 11, besidalijančių be liekanos iš ' + m1[j] +' yra ' + count + ' vienetai.');
+}
+console.log('---------------------------------------------------------------');
+
+count = 0;
+
+for(let j = 0;j<3;j++)
+{
+    for(let i = 8;i <= 31; i++){
+        if((i % m1[j])===0)
+        {
+            count++;
+        }
+    }
+    console.log('Skaičių intervale tarp 8 ir 31, besidalijančių be liekanos iš ' + m1[j] +' yra ' + count + ' vienetai.');
+}
+console.log('---------------------------------------------------------------');
+
+count = 0;
+
+for(let j = 0;j<3;j++)
+{
+    for(let i = -18;i <= 18; i++){
+        if((i % m1[j])===0)
+        {
+            count++;
+        }
+    }
+    console.log('Skaičių intervale tarp -18 ir 18, besidalijančių be liekanos iš ' + m1[j] +' yra ' + count + ' vienetai.');
+}
+console.log('---------------------------------------------------------------');
