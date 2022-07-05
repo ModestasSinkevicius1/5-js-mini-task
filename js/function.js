58ceca4c0aad22fd4b28cefde9138661d53abfeb
ModestasSinkevicius1 <modestas.sinkevicius1@gmail.com>
2022-07-05 10:19:51 +0300

    completed function tasks

--- js/function.js
+++ js/function.js
@@ -2,6 +2,8 @@
 
 // Uzduotis 1
 
+console.log('============================= Uzduotis 1 =============================\r\n')
+
 function tusciaFunction(){
     return false;
 }
@@ -10,8 +12,11 @@ console.log(tusciaFunction());
 console.log(tusciaFunction(0));
 console.log(tusciaFunction(Infinity));
 console.log(tusciaFunction(NaN));
+console.log('\r\n');
 
 // Uzduotis 2
+
+console.log('============================= Uzduotis 2 =============================\r\n')
 
 function daugyba(a, b){
     if(typeof a!== "number" || typeof b !== "number")
@@ -31,19 +36,120 @@ console.log(daugyba(1, NaN));
 console.log(daugyba(NaN, NaN));
 console.log(daugyba(Infinity, Infinity));
 console.log(daugyba(-Infinity, -Infinity));
+console.log('\r\n');
 
 // Uzduotis 3
+
+console.log('============================= Uzduotis 3 =============================\r\n')
 
 function skaitmenuKiekisSkaiciuje(a){
     if(typeof a !== "number" || !isFinite(a))
         return 'Pateikta netinkamo tipo reikšmė.'
     const textNumber = '' + a;
-    const numberLength = textNumber.length;
+    let numberLength = textNumber.length;
+    if(textNumber.includes('-'))
+    {
+        numberLength--;
+    }
     return numberLength;
 }
 
 console.log(skaitmenuKiekisSkaiciuje(1));
 console.log(skaitmenuKiekisSkaiciuje(1945));
+console.log(skaitmenuKiekisSkaiciuje(-1));
 console.log(skaitmenuKiekisSkaiciuje(Infinity));
 console.log(skaitmenuKiekisSkaiciuje(-Infinity));
 console.log(skaitmenuKiekisSkaiciuje(NaN));
+console.log(skaitmenuKiekisSkaiciuje('8'));
+console.log(skaitmenuKiekisSkaiciuje('abc'));
+console.log(skaitmenuKiekisSkaiciuje(true));
+console.log('\r\n');
+
+// Uzduotis 4
+
+console.log('============================= Uzduotis 4 =============================\r\n')
+
+function didziausiasSkaiciusSarase(a){
+    if(typeof a !== 'object' || a === null)
+        return 'Pateikta netinkamo tipo reikšmė.'
+    if(a.length <= 0)
+        return 'Pateiktas sąrašas negali būti tuščias.'
+    
+    let temp = a[0];
+
+    for(let i=1;i<a.length;i++)
+    {
+        if(temp<a[i])
+            temp = a[i];
+    }
+
+    return temp;
+}
+
+console.log(didziausiasSkaiciusSarase([1]));
+console.log(didziausiasSkaiciusSarase([1, 2, 3]));
+console.log(didziausiasSkaiciusSarase([-5, 78, 14, 0, 18]));
+console.log(didziausiasSkaiciusSarase([69, 69, 69, 69, 66]));
+console.log(didziausiasSkaiciusSarase([-1, -2, -3, -4, -5, -6, -7, -8]));
+console.log(didziausiasSkaiciusSarase('pomidoras'));
+console.log(didziausiasSkaiciusSarase([]));
+console.log(didziausiasSkaiciusSarase(null));
+console.log('\r\n');
+
+// Uzduotis 5
+
+console.log('============================= Uzduotis 5 =============================\r\n')
+
+function isrinktiRaides(text, row){
+    if(typeof text !== 'string')
+        return 'Pirmasis kintamasis yra netinkamo tipo.';
+    if(text.length <= 0 || text.length > 100)
+        return 'Pirmojo kintamojo reikšmė yra netinkamo dydžio.';
+    
+    if(typeof row !== 'number')
+        return 'Antrasis kintamasis yra netinkamo tipo.';
+    if(row <= 0)
+        return 'Antrasis kintamasis turi būti didesnis už nulį.';
+    if(row > text.length)
+        return 'Antrasis kintamasis turi būti ne didesnis už pateikto teksto ilgį.';
+
+    let junginys = '';
+    
+    for(let i=row-1;i<text.length;i+=row)
+    {
+        junginys += text[i];
+    }
+
+    return junginys;
+}
+
+console.log(isrinktiRaides('abcdefg', 2));
+console.log(isrinktiRaides('abcdefghijkl', 3));
+console.log(isrinktiRaides('abc', 0));
+console.log(isrinktiRaides('abc', 4));
+console.log( isrinktiRaides(1561, 2));
+console.log('\r\n');
+// Uzduotis 6
+
+console.log('============================= Uzduotis 6 =============================\r\n')
+
+function dalyba(a, b){
+    if(typeof a !== 'number' 
+    || typeof b !== 'number' 
+    || !isFinite(a) 
+    || !isFinite(b))
+        return 'Du kintamieji turi buti normalus skaiciai';
+    if(b === 0)
+        return 'Dalinimas iš nulio yra negalimas.';
+    const c = a / b;
+    return c;
+}
+
+console.log(dalyba(1, 2));
+console.log(dalyba(0, 2));
+console.log(dalyba(1, 0));
+console.log(dalyba('1', 2));
+console.log(dalyba(1, null));
+console.log(dalyba(-Infinity, 2));
+console.log(dalyba(NaN, 1));
+console.log(dalyba(1, [1, 2]));
