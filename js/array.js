74080aceb2b968dd81573d45650e724380eae135
ModestasSinkevicius1 <modestas.sinkevicius1@gmail.com>
2022-07-05 10:33:39 +0300

    completed number, string, arrays tasks

--- js/array.js
+++ js/array.js
@@ -1,1 +1,27 @@
-const yes = [1, 2];
+// Kintamuju inicijavimas su skaiciais
+const a = [1, 2, 3, 4, 5];
+console.log(a);
+
+const b = [0, 2, 4, 6, 8];
+console.log(b);
+
+const c = [1, 3, 5, 7, 9];
+console.log(c);
+
+// Kintamuju inicijavimas su tekstais
+const a1 = ['vienas','du', 'trys', 'keturi', 'penki'];
+console.log(a1);
+
+const b1 = ['Vilnius', 'Kaunas', 'Klaipeda', 'Panevezys', 'Siauliai'];
+console.log(b1);
+
+const c1 = ['Pomidoras', 'Agurkas', 'Burokas', 'Bulve', 'Morka'];
+console.log(c1);
+
+// Veiksmai su kintamaisiais
+
+const suma = a[0] - a[1] + a[2] - a[3] + a[4];
+console.log(suma);
+
+junginys = a1[0] + ', ' + a1[1] + ', ' + a1[2] + ', ' + a1[3] + ', ' + a1[4];
+console.log(junginys);
